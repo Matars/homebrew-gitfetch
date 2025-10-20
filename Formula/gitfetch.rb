c954aaf9a846a338e4e534964b0d785ba44cf757
km222uq <km222uq@student.lnu.se>
2025-10-20 15:06:58 +0200

    Initial commit for homebrew-gitfetch tap

---
+++ Formula/gitfetch.rb
@@ -0,0 +1,20 @@
+class Gitfetch < Formula
+  include Language::Python::Virtualenv
+
+  desc "A neofetch-style CLI tool for GitHub statistics"
+  homepage "https://github.com/Matars/gitfetch"
+  url "https://github.com/Matars/gitfetch/archive/refs/tags/v1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "PLACEHOLDER_SHA256"
+  license "GPL-2.0"
+
+  depends_on "python@3.8"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "#{bin}/gitfetch", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# Homebrew Gitfetch
+
+This is the official Homebrew tap for [gitfetch](https://github.com/Matars/gitfetch), a neofetch-style CLI tool for GitHub statistics.
+
+## Installation
+
+```bash
+brew install matars/homebrew-gitfetch/gitfetch
+```
+
+Or tap the repository first:
+
+```bash
+brew tap matars/homebrew-gitfetch
+brew install gitfetch
+```
+
+## Updating
+
+This tap is automatically updated when new releases are published on the main repository.
