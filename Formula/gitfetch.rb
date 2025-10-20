7c5574a507f7665073975e77a94f12515f7a15ef
km222uq <km222uq@student.lnu.se>
2025-10-20 18:34:49 +0200

    Fix requests SHA256 hash for correct dependency installation

--- Formula/gitfetch.rb
+++ Formula/gitfetch.rb
@@ -12,7 +12,7 @@ class Gitfetch < Formula
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
-    sha256 "de938129eaae1e34f0a182b4372fa4eaeebb613d584b81c52534e7aa43d61897"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   def install
