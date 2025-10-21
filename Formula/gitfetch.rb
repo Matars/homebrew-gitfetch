6e9096d3b63f56ebfc139876247815c36589be7a
km222uq <km222uq@student.lnu.se>
2025-10-22 01:21:42 +0200

    formula update

--- Formula/gitfetch.rb
+++ Formula/gitfetch.rb
@@ -16,7 +16,7 @@ class Gitfetch < Formula
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
-    sha256 "a4db93164c974401e757d65134f7eb1d2324e900bdb9260cc97a00897a7a4213"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
   end
 
   resource "idna" do
